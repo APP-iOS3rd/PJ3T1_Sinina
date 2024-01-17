76c5b4f05b92203b49a31c6de4c3ba5594218ab3
zoa0945 <zoa0945@zoa0945ui-MacBookAir.local>
2024-01-17 11:28:13 +0900

    feat: LoginView UI, UIColor extension 추가, 로그인 버튼 이미지 asset 수정

--- SininaCake/Scene/Login/LoginView.swift
+++ SininaCake/Scene/Login/LoginView.swift
@@ -9,7 +9,68 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Image("sininaCakeImage")
+                .resizable()
+                .frame(width: 120, height: 120)
+            
+            Spacer()
+            
+            HStack {
+                CustomText(
+                    title: "🍰 로그인하기",
+                    textColor: .black,
+                    textWeight: .semibold,
+                    textSize: 24
+                )
+                
+                Spacer()
+            }
+            .padding(.leading, 42)
+            
+            Spacer()
+                .frame(height: 32)
+            
+            LoginButtonView()
+            
+            Spacer()
+                .frame(height: 70)
+        }
+    }
+}
+
+struct LoginButtonView: View {
+    var body: some View {
+        VStack(spacing: 18) {
+            // 카카오 로그인 버튼
+            Button(action: {}, label: {
+                Image("kakaoLogin")
+                    .resizable()
+                    .scaledToFit()
+            })
+            .padding(.leading, 24)
+            .padding(.trailing, 24)
+            
+            // 구글 로그인 버튼
+            Button(action: {}, label: {
+                Image("googleLogin")
+                    .resizable()
+                    .scaledToFit()
+            })
+            .padding(.leading, 24)
+            .padding(.trailing, 24)
+            
+            // 애플 로그인 버튼
+            Button(action: {}, label: {
+                Image("appleLogin")
+                    .resizable()
+                    .scaledToFit()
+            })
+            .padding(.leading, 24)
+            .padding(.trailing, 24)
+        }
     }
 }
 
