0e46b37ed30eaa7f06d8ffe0bfd09f34477484e5
jake <88993810+jake5113@users.noreply.github.com>
2024-01-22 14:11:07 +0900

    feat: 카카오톡 로그인, 커스텀 버튼 오류 수정 (#6)
    * docs: 카카오 로그인 API 키값 Config 파일에 설정
    
    * fix: CustomButton 전체 영역 클릭 가능하게 변경
    
    * feat: 카카오 로그인 기초 설정
    
    * Delete SininaCake/ApiKeys.plist
    
    * Delete SininaCake/ApiKeys.xcconfig
    
    * test: Apple Login 테스트
    
    * chore: Apple 개발자 계정 인증서, 프로파일 적용
    
    * feat: 카카오 로그인 성공
    유저 프로필사진, 닉네임 획득
    
    * test: 카카오톡 인증 프사와 닉네임 획득, 로그아웃 테스트

--- SininaCake/Scene/Home/HomeView.swift
+++ SininaCake/Scene/Home/HomeView.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import KakaoSDKAuth
 
 struct HomeView: View {
+    @StateObject var homeVM = HomeViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // TODO: MyPage로 이동 예정
+        VStack {
+            NavigationStack {
+                NavigationLink (destination: LoginView()) {
+                    CustomButton(action: { homeVM.handleKakaoLogout() }, title: "로그아웃", titleColor: UIColor.white, backgroundColor: UIColor.customBlue, leading: 12, trailing: 12)
+                }
+            }
+            
+            AsyncImage(url: homeVM.profileImageURL)
+                .frame(width: 200, height: 200)
+            
+            Text(homeVM.nickname)
+        }
     }
 }
+
 
 #Preview {
     HomeView()
--- SininaCake/Scene/Login/LoginViewModel.swift
+++ SininaCake/Scene/Login/LoginViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import KakaoSDKUser
+
+class LoginViewModel: ObservableObject {
+    @Published var isLoggedin: Bool = false
+    
+    /// 카카오 로그인
+    func handleKakaoLogin() {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("loginWithKakaoTalk() success.")
+                    
+                    //do something
+                    
+                    self.isLoggedin = true
+                }
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("loginWithKakaoAccount() success.")
+                    
+                    //do something
+                    _ = oauthToken
+                    self.isLoggedin = true
+                }
+            }
+        }
+    }
+}
+
