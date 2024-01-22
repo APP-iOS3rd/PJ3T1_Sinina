634f55115a89e22bac50ba638fa6d469cf1ebbe5
sooobee <20subi@jbnu.ac.kr>
2024-01-22 14:53:03 +0900

    feat: ChatListView UI

--- SininaCake/Scene/Manager/ChatList/ChatListView.swift
+++ SininaCake/Scene/Manager/ChatList/ChatListView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ChatListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ScrollView {
+                    ForEach(0..<10, id: \.self){ num in
+                        VStack {
+                            HStack(spacing: 16) {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 32))
+                                VStack(alignment: .leading) {
+                                    Text("Username")
+                                    Text("Message sent to user")
+                                }
+                                Spacer()
+                                
+                                Text("22d")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            Divider()
+                        }.padding(.horizontal)
+                        
+                    }
+                }.navigationTitle("시니나케이크")
+            }
+        }
+        
     }
 }
 
