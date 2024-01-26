a2c57eca7d62918a3b29c1dde6780db0a7760a7b
jake <jongwon5113@gmail.com>
2024-01-26 17:39:01 +0900

    feat: 네이버 지도 구현 - 각자 로컬에서 'Pod init' 하고 시작 - ApiKeys.xcconfig 네이버 관련 키값 추가

--- SininaCake/Scene/Home/Map/MapView.swift
+++ SininaCake/Scene/Home/Map/MapView.swift
@@ -7,9 +7,59 @@
 
 import SwiftUI
 
+import NMapsMap
+
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            NaverMap(coord: (37.6550100, 127.069713))
+              .foregroundColor(.clear)
+              .frame(width: 382, height: 535)
+              .cornerRadius(12)
+              .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 8)
+              .padding(.horizontal, 24)
+            
+            Rectangle()
+            .foregroundColor(.clear)
+            .frame(width: 382, height: 100)
+            .background(.white)
+            .cornerRadius(12)
+            
+            VStack(alignment: .leading) {
+
+                CustomText(title: "시니나 케이크", textColor: .black, textWeight: .semibold, textSize: 18)
+                CustomText(title: "서울 노원구 노원로30길 44 1층", textColor: .customGray, textWeight: .semibold, textSize: 16)
+            }
+        }
+    }
+}
+
+struct NaverMap: UIViewRepresentable {
+    var coord: (Double, Double)
+  
+    func makeUIView(context: Context) -> NMFNaverMapView {
+        let view = NMFNaverMapView()
+        view.showZoomControls = false
+        view.mapView.positionMode = .direction
+        view.mapView.zoomLevel = 20
+        return view
+    }
+    
+    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
+        let coord = NMGLatLng(lat: coord.0, lng: coord.1)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+        cameraUpdate.animation = .fly
+        cameraUpdate.animationDuration = 1
+        uiView.mapView.moveCamera(cameraUpdate)
+        
+        let marker = NMFMarker()
+        
+        marker.position = coord
+        marker.mapView = uiView.mapView
+        marker.width = 40
+        marker.height = 50
+        marker.iconImage = NMF_MARKER_IMAGE_BLACK
+        marker.iconTintColor = .customBlue
     }
 }
 
