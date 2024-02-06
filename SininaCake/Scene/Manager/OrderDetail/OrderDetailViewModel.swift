f5a8dc437096cc31296b01a86a7c2c3a9d5aff3b
zoa0945 <51810980+zoa0945@users.noreply.github.com>
2024-02-06 18:23:30 +0900

    Feature/manager order view model (#32)
    * feat: Model 수정, OrderListVM fetchData로 firestoreDB 불러오기
    
    * feat: convert date to string and date to time
    
    * feat: OrderDetailView 가격, bottombutton UI
    
    * feat: OrderListVM, OrderDetailVM 로직 작성
    
    ---------
    
    Co-authored-by: zoa0945 <zoa0945@zoa0945ui-MacBookAir.local>

--- SininaCake/Scene/Manager/OrderDetail/OrderDetailViewModel.swift
+++ SininaCake/Scene/Manager/OrderDetail/OrderDetailViewModel.swift
@@ -6,4 +6,51 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseStorage
 
+class OrderDetailViewModel: ObservableObject {
+    let db = Firestore.firestore()
+    let storage = Storage.storage()
+    
+    func updatePrice(orderItem: OrderItem, _ price: Int) {
+//        db.collection("Users").document(orderItem.email).collection("Orders").document(orderItem.id).updateData(["confirmedPrice":price])
+        db.collection("CurrentOrders").document(orderItem.id).updateData(["confirmedPrice":price])
+    }
+    
+    func updateStatus(orderItem: OrderItem) {
+        switch orderItem.status {
+        case .notAssign:
+//            db.collection("Users").document(orderItem.email).collection("Orders").document(orderItem.id).updateData(["status":"승인"])
+            db.collection("CurrentOrders").document(orderItem.id).updateData(["status":"승인"])
+        case .assign:
+//            db.collection("Users").document(orderItem.email).collection("Orders").document(orderItem.id).updateData(["status":"완료"])
+            db.collection("CurrentOrders").document(orderItem.id).updateData(["status":"완료"])
+        default:
+            return
+        }
+    }
+    
+    func downloadImage(_ imageName: String) -> UIImage {
+        var image = UIImage()
+        
+        storage.reference(forURL: "gs://sininacake.appspot.com/\(imageName)").downloadURL { (url, error) in
+            guard let url = url else {
+                print("Cannot Found Firebase Storege url")
+                return
+            }
+            
+            guard let error = error else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let data = NSData(contentsOf: url)
+                image = UIImage(data: data! as Data)!
+            }
+        }
+        
+        return image
+    }
+}
--- SininaCake/Scene/Manager/OrderList/OrderListViewModel.swift
+++ SininaCake/Scene/Manager/OrderList/OrderListViewModel.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+import Firebase
+
+class OrderListViewModel: ObservableObject {
+    @Published var assignOrderData: [OrderItem] = []
+    @Published var notAssignOrderData: [OrderItem] = []
+    @Published var completeOrderData: [OrderItem] = []
+    
+    var db: Firestore!
+    var ordersRef: CollectionReference!
+    
+    init() {
+        let settings = FirestoreSettings()
+        
+        Firestore.firestore().settings = settings
+        
+        db = Firestore.firestore()
+        ordersRef = db.collection("CurrentOrders")
+    }
+    
+    func fetchData() {
+        assignOrderData = []
+        notAssignOrderData = []
+        completeOrderData = []
+        
+        let query: Query = ordersRef.order(by: "id")
+        
+        query.getDocuments { [weak self] querySnapshot, error in
+            if let error: Error = error {
+                fatalError("Error getting documents: \(error.localizedDescription)")
+            } else if querySnapshot?.documents.isEmpty == true {
+                // snapshot이 비어있을 때
+                
+            } else {
+                if let snapshot = querySnapshot, let self = self {
+                    for doc in snapshot.documents {
+                        let documentData: [String: Any] = doc.data()
+                        print(documentData)
+                        
+                        let id: String = documentData["id"] as? String ?? ""
+                        let email: String = documentData["email"] as? String ?? ""
+                        let date: Timestamp = documentData["date"] as? Timestamp ?? Timestamp()
+                        let orderTime: Timestamp = documentData["orderTime"] as? Timestamp ?? Timestamp()
+                        let cakeSize: String = documentData["size"] as? String ?? ""
+                        let sheet: String = documentData["sheet"] as? String ?? ""
+                        let cream: String = documentData["cream"] as? String ?? ""
+                        let icePack: Bool = documentData["icePack"] as? Bool ?? false
+                        let name: String = documentData["name"] as? String ?? ""
+                        let phoneNumber: String = documentData["phoneNumber"] as? String ?? ""
+                        let text: String = documentData["text"] as? String ?? ""
+                        let imageURL: [String] = documentData["imageURL"] as? [String] ?? []
+                        let comment: String = documentData["comment"] as? String ?? ""
+                        let expectedPrice: Int = documentData["expectedPrice"] as? Int ?? 0
+                        let confirmedPrice: Int = documentData["confirmedPrice"] as? Int ?? 0
+                        let status: String = documentData["status"] as? String ?? ""
+                        
+                        let orderDate = OrderItem(id: id, email: email, date: self.timestampToDate(date), orderTime: self.timestampToDate(orderTime), cakeSize: cakeSize, sheet: sheet, cream: cream, icePack: icePack, name: name, phoneNumber: phoneNumber, text: text, imageURL: imageURL, comment: comment, expectedPrice: expectedPrice, confirmedPrice: confirmedPrice, status: self.stringToStatus(status))
+                        
+                        if self.stringToStatus(status) == .assign {
+                            assignOrderData.append(orderDate)
+                        } else if self.stringToStatus(status) == .notAssign {
+                            notAssignOrderData.append(orderDate)
+                        } else if self.stringToStatus(status) == .complete {
+                            completeOrderData.append(orderDate)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func timestampToDate(_ date: Timestamp) -> Date {
+        return date.dateValue()
+    }
+    
+    private func stringToStatus(_ status: String) -> OrderStatus {
+        switch status {
+        case "승인":
+            return .assign
+        case "미승인":
+            return .notAssign
+        case "완료":
+            return .complete
+        default:
+            return .notAssign
+        }
+    }
+}
