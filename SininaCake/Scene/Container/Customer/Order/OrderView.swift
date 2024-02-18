94b7ae620c55e443f4dda901f3f5c93780d38346
zoa0945 <51810980+zoa0945@users.noreply.github.com>
2024-02-18 23:45:17 +0900

    fix: storage 이미지 경로, UI 일부 수정 (#80)
    * fix: OrderStatusView 이미지 크기 수정, 이미지 다운로드 경로 수정
    
    * fix: ProfileView, OrderStatusView fetchData 위치 변경
    
    * fix: FCM message 텍스트 수정
    
    ---------
    
    Co-authored-by: zoa0945 <zoa0945@zoa0945ui-MacBookAir.local>
    Co-authored-by: jake <88993810+jake5113@users.noreply.github.com>

--- SininaCake/Scene/Container/Customer/Order/OrderView.swift
+++ SininaCake/Scene/Container/Customer/Order/OrderView.swift
@@ -628,8 +628,21 @@ private struct BottomView: View {
 
             }
 
-            
-            CustomButton(action: {defer {orderData.addOrderItem()}; for i in 0...photoVM.selectedImages.count - 1 {photoVM.uploadPhoto(i, orderData.orderItem.id); orderData.imgURL(i)}; orderData.orderItem.expectedPrice = orderData.expectedPrice(); orderData.orderItem.email = loginVM.loginUserEmail ?? ""}, title: "예약하기", titleColor: orderData.isallcheck() && !photoVM.selectedImages.isEmpty ? .white : .customDarkGray, backgroundColor: orderData.isallcheck() && !photoVM.selectedImages.isEmpty ? .customBlue : .textFieldColor, leading: 110, trailing: 24)
+            CustomButton(action: {
+                defer {
+                    orderData.addOrderItem()
+                }
+                for i in 0...photoVM.selectedImages.count - 1 {
+                    photoVM.uploadPhoto(i, orderData.orderItem.id)
+                    orderData.imgURL(i)
+                }
+                orderData.orderItem.expectedPrice = orderData.expectedPrice()
+                orderData.orderItem.email = loginVM.loginUserEmail ?? ""
+            },
+                         title: "예약하기",
+                         titleColor: .white, 
+                         backgroundColor: orderData.isallcheck() && !photoVM.selectedImages.isEmpty ? .customBlue : .textFieldColor, leading: 110, trailing: 24
+            )
                 .kerning(0.45)
                 .padding(.vertical, (UIScreen.main.bounds.height) * 12/932)
                 .disabled(!orderData.isallcheck() || photoVM.selectedImages.isEmpty)
