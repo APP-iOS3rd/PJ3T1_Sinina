b37af6231005e71dbf9b94f2b19fc4ee655cae5a
danzi145 <120579315+danzi145@users.noreply.github.com>
2024-02-20 15:23:18 +0900

    Feature/CalendarView3 (#85)
    * feat: 날짜색변경저장
    
    * feat:DateValueModel
    
    * feat: Codable 수정
    
    * feat: CalendarViewModel 수정
    
    * feat: viewmodel 이름변경
    
    * feat: CalendarViewModel 수정
    
    * feat: CalendarViewModel 수정
    
    * feat: CalendarView,VM 수정
    
    로직 수정
    -뷰가 켜질 때, 저장된 데이터 반영
    - 기존에 데이터가 monthoffset변화에 따라 로드되도록 수정
    - 기타 같은 달에 칸 수를 맞추기 위해 이전달의 날짜와 이후달의 날짜가 같은 day값일 경우 날짜의 색깔여부가 같이 바뀌는 오류 수정
    
    코드 공백 줄이고 필요없는 코드 삭제
    
    * feat: CalendarView 오류수정
    
    * feat: CalendarViewModel 로직 수정
    
    * feat: CalendarView 로직수정
    
    * feat: CalendarView 로직수정
    
    calendarListView 추가 및 calendarView 로직 수정
    
    * feat:CalendarView 수정
    
    * feat: CalendarView 수정
    
    자잘한 오류 수정,
    CalendarViewModel id 초기값 삭제
    
    * feat: CalendarView 오류 수정
    
    ManagerOnlyView에 뷰가 onchange 되지 않는 오류 수정
    
    ---------
    
    Co-authored-by: jake <88993810+jake5113@users.noreply.github.com>
    Co-authored-by: jake5113 <jongwon5113@gmail.com>

--- SininaCake/Scene/Manager/Calendar/CalendarView.swift
+++ SininaCake/Scene/Manager/Calendar/CalendarView.swift
@@ -5,58 +5,91 @@
 //  Created by  zoa0945 on 11/12/23.
 //
 import SwiftUI
+import FirebaseAuth
+import KakaoSDKAuth
 struct CalendarView: View {
     
     @Environment(\.sizeCategory) var sizeCategory
-    @ObservedObject var dateValueVM = DateValueViewModel()
-    var dateString: String? {
-        let date =  Date()                     // 넣을 데이터(현재 시간)
-        let myFormatter = DateFormatter()
-        myFormatter.dateFormat = "MM-dd"  // 변환할 형식
-        let dateString = myFormatter.string(from: date)
-        return dateString
-    }
+    @ObservedObject var calendarVM = CalendarViewModel()
+    
+    @StateObject var calendarListVM = CalendarListViewModel()
+    @StateObject var loginVM = LoginViewModel()
+    @State private var selectedDate: Date?
+    
     var testSchedule = Schedule(name: "", startDate: Date(), endDate: Calendar.current.date(byAdding: .day, value: 2, to: Date()) ?? Date())
     @State var currentDate = Date()
     @State var daysList = [[DateValue]]()
     //화살표 클릭에 의한 월 변경 값
     @State var monthOffset = 0
+    @State var edit: Bool = false
+    @State var getData: Bool = false
     
     var body: some View {
-        VStack() {
-            Rectangle()
-                .foregroundColor(.clear)
-                .frame(width: 342, height: 441)
-                .background(
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .cornerRadius(12)
-                            .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 8)
-                        VStack() {
-                            headerView
-                            Divider()
-                                .frame(width: 302)
-                            weekView
-                            cardView
-                            Divider()
-                                .frame(width: 302)
-                            bookingView
-                                .padding([.horizontal,.vertical], 24)
+        ScrollView {
+            
+            Spacer()
+            Spacer()
+            VStack() {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 342, height: 441)
+                    .background(
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .cornerRadius(12)
+                                .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 8)
+                            VStack() {
+                                headerView
+                                Divider()
+                                    .frame(width: 302)
+                                weekView
+                                cardView
+                                Divider()
+                                    .frame(width: 302)
+                                bookingView
+                                    .padding([.horizontal,.vertical], 24)
+                            }
                         }
+                    )
+            }
+            VStack {
+                if edit == true {
+                    if let selectedDate = selectedDate {
+                        
+                        let formattedDateString = convert(date: selectedDate)
+                        CalListView(orderData: calendarListVM.allOrderData.filter { dateToString($0.date).contains(formattedDateString) }, title: "주문 내역", titleColor: .black)
+                        let _ = print(formattedDateString)
                     }
-                )
+                }
+            }
+            .onAppear {
+                calendarListVM.fetchData()
+                calendarVM.removePastDateValues()
+                print("view 로드, 현재시간 이전 데이터 삭제")
+            }
+            
         }
+    }
+    
+    func convert(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        return dateFormatter.string(from: date)
     }
     
     private var headerView: some View {
         HStack {
+            Spacer()
+            Spacer()
+            
             Button {
                 monthOffset -= 1
                 
             } label: {
                 Image("angle-left")
             }
+            .offset(x: 5)
             Text(month())
                 .font(
                     Font.custom("Pretendard", fixedSize: 24)
@@ -65,13 +98,37 @@ struct CalendarView: View {
                 .foregroundColor(Color(red: 0.45, green: 0.76, blue: 0.87))
                 .minimumScaleFactor(0.7)
                 .padding()
+                .offset(x: 5)
             Button {
                 monthOffset += 1
             } label: {
                 Image("angle-right")
             }
+            .offset(x: 5)
+            
+            Spacer()
+            
+            if loginVM.isManager {
+                Button {
+                    print("편집 작동, \(edit)")
+                    edit.toggle()
+                    getData.toggle()
+                    
+                } label: {
+                    Image(systemName:"list.clipboard.fill")
+                        .foregroundColor(Color(.customBlue))
+                }
+            }
+            
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity) // 부모 스택의 크기를 가득 채우도록 설정
+        .task {
+            // 로그인된 사용자의 이메일로 매니저 여부 확인
+            if let userEmail = Auth.auth().currentUser?.email {
+                await loginVM.checkManager(email: userEmail)
+            }
+        }
     }
     
     private var weekView: some View {
@@ -98,53 +155,72 @@ struct CalendarView: View {
             ForEach(daysList.indices, id: \.self) { i in
                 HStack() {
                     ForEach(daysList[i].indices, id: \.self) { j in
-                        CardView(value: $daysList[i][j], schedule: testSchedule, dateValueViewModel:dateValueVM,isReadOnly: false)
+                        CardView(value: $daysList[i][j], schedule: testSchedule, calendarVM:calendarVM, edit: $edit, getData: $getData, loginVM: loginVM, calendarListVM: calendarListVM) { selectedDateValue in
+                            handleDateClick(dateValue: selectedDateValue)
+                        }
+                        
                     }
                 }
                 .minimumScaleFactor(0.1)
             }
         }
         .onDisappear()
+        .onAppear() {
+            monthOffset = Int(month()) ?? 0
+            currentDate = getCurrentMonth()
+            daysList = extractDate()
+            calendarVM.loadDataFromFirestore()
+            print("onappear - 캘린더뷰")
+            print("onappear \(loginVM.isManager)")
+            for dv in calendarVM.dateValues {
+                if currentDate.month == dv.date.month {
+                    print("onchange - month : \(dv.date.month)")
+                    for i in daysList.indices {
+                        for j in daysList[i].indices {
+                            if !daysList[i][j].isNotCurrentMonth && daysList[i][j].day == dv.day {
+                                daysList[i][j] = dv
+                            }
+                        }
+                    }
+                }
+            }
+        }
         .onChange(of: monthOffset) { _ in
             // updating Month...
             print("onchange - monthoffset, \(monthOffset)")
             currentDate = getCurrentMonth()
             daysList = extractDate()
-            dateValueVM.loadDataFromFirestore()
-            for dv in dateValueVM.dateValues {
+            calendarVM.loadDataFromFirestore()
+            for dv in calendarVM.dateValues {
                 if currentDate.month == dv.date.month {
                     print("onchange - month : \(dv.date.month)")
                     for i in daysList.indices {
                         for j in daysList[i].indices {
-                                if !daysList[i][j].isNotCurrentMonth && daysList[i][j].day == dv.day {
-                                    daysList[i][j] = dv
-                                }
+                            if !daysList[i][j].isNotCurrentMonth && daysList[i][j].day == dv.day {
+                                daysList[i][j] = dv
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+        .onChange(of:calendarVM.dateValues) { _ in
+            print("onchange - dataValues , \(calendarVM.dateValues.count)")
+            for dv in calendarVM.dateValues {
+                if currentDate.month == dv.date.month {
+                    print("onchange - month : \(dv.date.month)")
+                    for i in daysList.indices {
+                        for j in daysList[i].indices {
+                            if !daysList[i][j].isNotCurrentMonth && daysList[i][j].day == dv.day {
+                                daysList[i][j] = dv
+                            }
                         }
                     }
                 }
             }
         }
-        .onChange(of:dateValueVM.dateValues) { _ in
-            print("onchange - dataValues , \(dateValueVM.dateValues.count)")
-            for dv in dateValueVM.dateValues {
-                if currentDate.month == dv.date.month {
-                    print("onchange - month : \(dv.date.month)")
-                    for i in daysList.indices {
-                        for j in daysList[i].indices {
-                                if !daysList[i][j].isNotCurrentMonth && daysList[i][j].day == dv.day {
-                                    daysList[i][j] = dv
-                                }
-                        }
-                    }
-                }
-            }
-        }
-        .onAppear() {
-            monthOffset = Int(month()) ?? 0
-            currentDate = getCurrentMonth()
-            daysList = extractDate()
-            dateValueVM.loadDataFromFirestore()
-        }
+        
     }
     
     private var bookingView: some View {
@@ -184,6 +260,23 @@ struct CalendarView: View {
                         .stroke(Color(red: 0.6, green: 0.6, blue: 0.6), lineWidth: 1))
         }
     }
+    
+    
+    private func handleDateClick(dateValue: DateValue) {
+        // 날짜기준 ListView 데이터 필터링
+        // 예시로 "yyyy/MM/dd" 형식의 문자열을 기준으로 필터링하도록 구현
+        selectedDate = dateValue.date.withoutTime()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        let dateString = dateFormatter.string(from: dateValue.date)
+        let filteredOrders = calendarListVM.allOrderData.filter { order in
+            dateToString(order.date).contains(dateString)
+        }
+        // 필터링된 데이터를 사용하여 UI 업데이트 등을 수행
+        // 여기에서는 print만 수행
+        print("Filtered Orders for \(dateString): \(filteredOrders)")
+    }
+    
     //현재 날짜 년도
     func year() -> String {
         let formatter = DateFormatter()
@@ -261,19 +354,22 @@ struct CalendarView: View {
 }
 
 struct CardView: View {
-    
     @Binding var value: DateValue
     @State var schedule: Schedule
-    @ObservedObject var dateValueViewModel: DateValueViewModel
-    @State var isReadOnly: Bool
-    
-    func selectedDate2() {
-        if isReadOnly == false {
+    @ObservedObject var calendarVM: CalendarViewModel
+    @Binding var edit: Bool
+    @Binding var getData: Bool
+    @StateObject var loginVM: LoginViewModel
+    @StateObject var calendarListVM: CalendarListViewModel
+    var onDateClick: (DateValue) -> Void
+    func selectedDate() {
+        if loginVM.isManager {
             value.selectedToggle()
             // 클릭할 때마다 클릭 여부를 변경
             print("tap\(value.isSelected)")
         }
     }
+
     var body: some View {
         ZStack() {
             HStack {
@@ -290,10 +386,15 @@ struct CardView: View {
                                 .foregroundColor(value.isSelected ? Color(UIColor.customBlue) : (value.isSecondSelected ? Color(UIColor.customDarkGray) : Color(UIColor.customRed)))
                                 .padding([.leading, .bottom], 10)
                                 .onTapGesture {
-                                    selectedDate2()
-                                    let dateValue = DateValue(day: value.day, date: value.date.withoutTime())
-                                    value.saveDateValueToFirestore(dateValue: value)
-                                    dateValueViewModel.removeDuplicateDay(dateValue: dateValue)
+                                    if edit == false {
+                                        selectedDate()
+                                        let dateValue = DateValue(day: value.day, date: value.date.withoutTime())
+                                        value.saveDateValueToFirestore(dateValue: value)
+                                        calendarVM.removeDuplicateDay(dateValue: dateValue)
+                                    } else {
+                                        onDateClick(value)
+                                        print("\(value) 클릭")
+                                    }
                                 }
                             
                         } else if schedule.startDate.withoutTime() == value.date {
@@ -318,12 +419,16 @@ struct CardView: View {
                                 .foregroundColor((value.date.weekday == 1 || value.date.weekday == 2) ? (value.isSelected ? Color(UIColor.customBlue) : (value.isSecondSelected ? Color(UIColor.customRed) : Color(UIColor.customDarkGray))) : (value.isSelected ? Color(UIColor.customDarkGray) : (value.isSecondSelected ? Color(UIColor.customRed) : Color(UIColor.customBlue))))
                                 .padding([.leading, .bottom], 10)
                                 .onTapGesture {
-                                    selectedDate2()
-                                    let dateValue = DateValue(day: value.day, date: value.date.withoutTime())
-                                    value.saveDateValueToFirestore(dateValue: value)
-                                    dateValueViewModel.removeDuplicateDay(dateValue: dateValue)
+                                    if edit == false  {
+                                        selectedDate()
+                                        let dateValue = DateValue(day: value.day, date: value.date.withoutTime())
+                                        value.saveDateValueToFirestore(dateValue: value)
+                                        calendarVM.removeDuplicateDay(dateValue: dateValue)
+                                    } else {
+                                        onDateClick(value)
+                                        print("\(value) 클릭")
+                                    }
                                 }
-                            
                         }
                     }
                 }
@@ -331,6 +436,11 @@ struct CardView: View {
         }
         .frame(width: UIScreen.main.bounds.width / 13)
         .frame(height: 40)
+        .task {
+            if let userEmail = Auth.auth().currentUser?.email {
+                await loginVM.checkManager(email: userEmail)
+            }
+        }
     }
 }
 
