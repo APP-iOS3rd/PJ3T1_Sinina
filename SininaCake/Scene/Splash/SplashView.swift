94bc1f50ec0f0752b813f95e9dff50537912c0f5
jake <88993810+jake5113@users.noreply.github.com>
2024-03-17 02:05:43 +0900

    Feature: 앱 버전 체크 및 업데이트 강제화 (#127)
    * docs: spm version update
    
    * feat: 앱 업데이트 체커 구현

--- SininaCake/Scene/Splash/SplashView.swift
+++ SininaCake/Scene/Splash/SplashView.swift
@@ -12,20 +12,36 @@ import KakaoSDKAuth
 struct SplashView: View {
     @StateObject var loginVM = LoginViewModel.shared
     @StateObject var splashVM = SplashViewModel()
-    @State var changeView = false
+    @State private var changeView = false
+    @State private var showUpdate: Bool = false
     
     var body: some View {
         Image("sininaCakeImage")
             .resizable()
             .frame(width: 180, height: 180)
             .onAppear {
-                Task {
-                    await splashVM.fetchUserData()
-                    changeView = true
+                Task {    
+                    if await AppstoreVersionChecker.isNewVersionAvailable() {
+                        showUpdate.toggle()
+                    } else {
+                        await splashVM.fetchUserData()
+                        changeView = true
+                    }
+                }
+            }
+            .alert("새로운 버전 업데이트가 있어요", isPresented: $showUpdate) {
+                Button("종료하기") {
+                    UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        exit(0)
+                    }
+                }
+                if let url = URL(string: "itms-apps://itunes.apple.com/app/apple-store/6476194031") {
+                    Link("업데이트", destination: url)
                 }
             }
             .fullScreenCover(isPresented: $changeView) {
-                if loginVM.loginUserEmail != nil {
+                 if loginVM.loginUserEmail != nil {
                     ContainerView().environmentObject(loginVM)
                 } else {
                     LoginView().onOpenURL(perform: { url in
